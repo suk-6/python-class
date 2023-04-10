10e2f6a36546cc950ae3f3ee339058446fe03642
Woosuk Nam <admin@suk.kr>
2023-04-10 09:30:15 +0900

    Create 230410.py

---
+++ 230410.py
@@ -0,0 +1,9 @@
+num = [i for i in range(1, 11)]
+num2 = [i for i in range(2, 21, 2)]
+
+nums = [0, 1, 2, 100, 10]
+nums.sort()
+nums.reverse
+
+tuple1 = (0,1,2,3,('a','b','c'), 5)
+print(tuple1[2], tuple1[4])
