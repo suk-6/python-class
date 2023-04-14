a66568743c42cbcc8fae2763d419029a74809167
Woosuk Nam <admin@suk.kr>
2023-04-14 10:41:18 +0900

    Create 230414.py

---
+++ 230414.py
@@ -0,0 +1,9 @@
+string = "Hello, World!"
+
+right_char1 = string[7:]
+right_char2 = string[-6:]
+print(right_char1, right_char2)
+
+right_char1 = string[:6]
+right_char2 = string[:-8]
+print(right_char1, right_char2)
