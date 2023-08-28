f94cd041419da591eddfd0dc54f3fbd57cad9f1f
suk-6 <me@suk.kr>
2023-08-28 10:54:19 +0900

    Class: 230828

---
+++ 230828.py
@@ -0,0 +1,22 @@
+def hello():
+    print("Hello")
+
+
+hello()
+hello()
+
+
+def order(item):
+    print(item, "주문하였습니다.")
+
+
+order(input("주문하실 음료를 알려주세요\n"))
+
+
+def star():
+    print("*****")
+
+
+star()
+star()
+star()
